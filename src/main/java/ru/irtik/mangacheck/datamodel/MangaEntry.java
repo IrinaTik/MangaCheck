eccd29baa016f98d71160e14cd6415e72fa73024
IrinaTik <60540077+IrinaTik@users.noreply.github.com>
2020-09-02 18:47:58 +0300

    1. Добавлен класс MangaEntry, описывающий мангу 2. Добавлено небольшое описание плана работ в readme.

---
+++ src/main/java/ru/irtik/mangacheck/datamodel/MangaEntry.java
@@ -0,0 +1,75 @@
+package ru.irtik.mangacheck.datamodel;
+
+import java.util.Objects;
+
+public class MangaEntry {
+
+    private String title;
+    private int chapterCount;
+    private int lastChapter;
+
+    public enum mangaStatus {
+        COMPLETE, ONGOING, FROZEN, DROPPED, DELETED
+    }
+
+    private mangaStatus status;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public MangaEntry withTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public int getChapterCount() {
+        return chapterCount;
+    }
+
+    public MangaEntry withChapterCount(int chapterCount) {
+        this.chapterCount = chapterCount;
+        return this;
+    }
+
+    public int getLastChapter() {
+        return lastChapter;
+    }
+
+    public MangaEntry withLastChapter(int lastChapter) {
+        this.lastChapter = lastChapter;
+        return this;
+    }
+
+    public mangaStatus getStatus() {
+        return status;
+    }
+
+    public MangaEntry withStatus(mangaStatus status) {
+        this.status = status;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MangaEntry that = (MangaEntry) o;
+        return chapterCount == that.chapterCount &&
+                lastChapter == that.lastChapter &&
+                title.equals(that.title) &&
+                status == that.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, chapterCount, lastChapter, status);
+    }
+
+    @Override
+    public String toString() {
+        return "MangaEntry{" +
+                "title='" + title + '\'' +
+                '}';
+    }
+}
